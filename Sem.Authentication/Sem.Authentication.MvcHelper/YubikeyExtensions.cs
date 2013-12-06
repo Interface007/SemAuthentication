2b3506abbff5f72851e34ce66f5b9270addd5edc
Matzen <sven.erik.matzen@web.de>
2013-12-06 07:41:39 +0100

    Started with In-App-IPS

--- Sem.Authentication/Sem.Authentication.MvcHelper/YubikeyExtensions.cs
+++ Sem.Authentication/Sem.Authentication.MvcHelper/YubikeyExtensions.cs
@@ -18,21 +18,39 @@ namespace Sem.Authentication.MvcHelper
     public static class YubikeyExtensions
     {
         /// <summary>
+        /// The yubikey input box markup.
+        /// </summary>
+        private const string YubikeyInputBox = "<input class=\"yubikeyInput\" id=\"yubiKey\" name=\"yubiKey\" text=\"\"/>";
+
+        /// <summary>
         /// Renders an input tag for the YUBIKEY OTP and an image tag as a "caption".
         /// </summary>
-        /// <param name="view"> The view. </param>
-        /// <param name="targetAction"> The target action. </param>
+        /// <param name="htmlHelper"> The htmlHelper. </param>
+        /// <param name="targetAction"> The target action or target url. If you want a full URL to use, make sure you did add the protocol (https/http). </param>
         /// <returns> The <see cref="IHtmlString"/>. </returns>
-        public static IHtmlString YubikeyInput(this HtmlHelper view, string targetAction)
+        public static IHtmlString YubikeyInput(this HtmlHelper htmlHelper, string targetAction)
         {
             YubikeyConfiguration.DeserializeConfiguration().EnsureCorrectConfiguration();
 
+            if (!targetAction.Contains(":"))
+            {
+                var controller = (System.Web.Mvc.Controller)htmlHelper.ViewContext.Controller;
+                targetAction = controller.Url.Action(targetAction);
+            }
+
             var imgUrl = targetAction + "?42FE943EC8A64735A978D1F81D5FFD00";
+            return new HtmlString("<img class=\"yubikeyCaption\" alt=\"YubiKey: \" id=\"yubiKeyImage\" src=\"" + imgUrl + "\" />" + YubikeyInputBox);
+        }
 
-            var text = "<img alt=\"YubiKey: \" id=\"yubiKeyImage\" src=\"" + imgUrl + "\" />" +
-                       "<input id=\"yubiKey\" name=\"yubiKey\" text=\"\"/>";
-
-            return new HtmlString(text);
+        /// <summary>
+        /// Renders an input tag for the YUBIKEY OTP and a SPAN tag as a "caption".
+        /// </summary>
+        /// <param name="htmlHelper"> The htmlHelper. </param>
+        /// <returns> The <see cref="IHtmlString"/>. </returns>
+        public static IHtmlString YubikeyInput(this HtmlHelper htmlHelper)
+        {
+            YubikeyConfiguration.DeserializeConfiguration().EnsureCorrectConfiguration();
+            return new HtmlString("<span class=\"yubikeyCaption\" >YubiKey: <span/>" + YubikeyInputBox);
         }
     }
 }
